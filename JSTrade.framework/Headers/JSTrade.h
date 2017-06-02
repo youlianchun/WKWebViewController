e5e56b8beb89e3da59ff014e0c827011aebddd65
YLCHUN <985050622@qq.com>
2017-06-02 11:16:45 +0800

    no message

--- JSTrade.framework/Headers/JSTrade.h
+++ JSTrade.framework/Headers/JSTrade.h
@@ -5,18 +5,20 @@
 //  Created by YLCHUN on 2017/5/29.
 //  Copyright © 2017年 ylchun. All rights reserved.
 //
-//  Other linker flags  -ObjC
 
+#define FOUNDATION_IMPORT_JSTRADE void import_JSTrade();\
+__attribute__((used)) static void importJSTrade () {\
+    import_JSTrade();\
+}
 
 #import "JSExport.h"
 #import "JSImport.h"
+
+FOUNDATION_IMPORT_JSTRADE
 
 //! Project version number for JSTrade.
 FOUNDATION_EXPORT double JSTradeVersionNumber;
 
 //! Project version string for JSTrade.
 FOUNDATION_EXPORT const unsigned char JSTradeVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <JSTrade/PublicHeader.h>
-
 
